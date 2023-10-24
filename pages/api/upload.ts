e0c0cd88b7a11c3384082e02646aa4e0c1be91cf
Dylan Bowman <dylanbowmandsk@gmail.com>
2023-10-24 03:57:14 +0100

    cache cleared

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client"
+import prisma from "../../prisma/prisma"
 
 export const config = {
     api: {
@@ -9,15 +9,14 @@ export const config = {
 }
 
 export default async function POST(req, res){
-    const prisma = new PrismaClient()
     const data = await prisma.shirt.create({data: {
-        created: new Date(),
         player: req.body.player,
         number: parseInt(req.body.number),
         match: req.body.match,
         home: req.body.home == 1 ? true : false ,
         date: new Date(req.body.date),
         path: req.body.path,
+        created: req.body.created
     }
     })
     res.status(200).json({ data: "success" })
